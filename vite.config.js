b10c72715936fef0b7ca3376f0ee48d724b4b3cf
Facundo Seib <fseib@trans.com.ar>
2026-06-18 15:23:46 -0300

    vite.config.js para pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bauhaus-f1/', // 🔥 REEMPLAZÁ ACA con el nombre exacto de tu repo de GitHub
 })
